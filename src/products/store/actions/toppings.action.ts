9e82496abca687e19b8ce0394644da33d889502a
Roman Dushko <rodush@gmail.com>
2017-12-14 13:22:16 +0100

    Further Action Creators: done

---
+++ src/products/store/actions/toppings.action.ts
@@ -0,0 +1,23 @@
+import { Action } from '@ngrx/store';
+
+import { Topping } from '../../models/topping.model';
+
+export const LOAD_TOPPINGS = '[Products] Load Toppings';
+export const LOAD_TOPPINGS_SUCCESS = '[Products] Load Toppings Succeed';
+export const LOAD_TOPPINGS_FAIL = '[Products] Load Toppings Failed';
+
+export class LoadToppings implements Action {
+    readonly type = LOAD_TOPPINGS;
+}
+
+export class LoadToppingsSuccess implements Action {
+    readonly type = LOAD_TOPPINGS;
+    constructor(public payload: any) {}
+}
+
+export class LoadToppingsFail implements Action {
+    readonly type = LOAD_TOPPINGS;
+    constructor(public payload: any) {}
+}
+
+export type ToppingsAction = LoadToppings | LoadToppingsSuccess | LoadToppingsFail;
